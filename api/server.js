5d7635118f645f19467fdf380091d465d711fc58
Faye Skeen <fskeen@gmail.com>
2019-09-20 09:07:57 -0700

    Register works.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const dbConfig = require('../database/dbConfig');
 
 const server = express();
+
+const sessionConfig = {
+    name: 'chocochip',
+    secret: process.env.SESSION_SECRET || 'WOLOLO',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KnexSessionStore({
+        knex: dbConfig, 
+        createtable: true,
+        tablename: 'knexsessions',
+        sidfieldname: 'sessionsid',
+        clearInterval: 1000 * 60 * 30,
+    })
+};
+server.use(session(sessionConfig));
 
 server.use(helmet());
 server.use(cors());
